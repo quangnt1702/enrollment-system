74874cb81cbc6071e6e0e28d3255a53049d6ccfa
quangnt1702 <quangnt1702@gmail.com>
2021-10-31 10:10:28 +0700

    Test

--- DataAccess/Repository/ICourseRepository.cs
+++ DataAccess/Repository/ICourseRepository.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccess.Models;
 
 namespace DataAccess.Repository
 {
-    class ICourseRepository
+    public interface ICourseRepository
     {
+        IEnumerable<Course> GetCourses();
+        Course GetCourseByID(int courseId);
+        void DeleteCourse(int courseId);
+        void InsertCourse(Course course);
+        void UpdateCourse(Course course);
     }
 }
--- EnrollmentSystemApp/frmStudentCourses.cs
+++ EnrollmentSystemApp/frmStudentCourses.cs
@@ -7,14 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DataAccess.Models;
+using DataAccess.Repository;
 
 namespace EnrollmentSystemApp
 {
     public partial class frmStudentCourses : Form
     {
+        ICourseRepository courseRepository = new CourseRepository();
         public frmStudentCourses()
         {
             InitializeComponent();
+        }
+        public void LoadCourseList()
+        {
+            var courseList = courseRepository.GetCourses();
+            var list = (from c in courseList
+                        select new
+                        {
+                            ID = c.CourseId,
+                            Name = c.CourseName,
+                            Subject = c.Subject.SubjectName,
+                            Lecture = c.Lecturer.UserName,
+                            Quantity = c.StudentQuantity,
+                            StartDate = c.StartDate,
+                            EndDate = c.EndDate,
+                            Status = c.Status.StatusName
+                        }).ToList();
+            dgvCourseList.DataSource = null;
+            dgvCourseList.DataSource = list;
+        }
+
+        private void frmStudentCourses_Load(object sender, EventArgs e)
+        {
+            LoadCourseList();
+        }
+
+        private void btnAllCourses_Click(object sender, EventArgs e)
+        {
+            LoadCourseList();
         }
     }
 }
